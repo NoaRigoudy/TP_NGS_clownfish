63a39b1b0d8929fd158ecc5386c5f0376d301129
noatheclownfish <noa.rigoudy@ens-lyon.fr>
2019-11-05 15:56:47 +0000

    Recuperation subset de data de sequencage en fichiersfastq

--- src/sra_download.sh
+++ src/sra_download.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
 # Create a working directory:
-data="/mnt/data/Aoce"
-mkdir -p $data
+data="/home/rstudio/disk/data"
+mkdir -p $data    
 cd $data
+
+# -p permet de créer un dossier "data"" s'il n'existe pas deja 
+
 
 # Create a directory where the data will be downloaded
 mkdir -p sra_data
 cd sra_data
 
+
 # Make a list of SRR accessions:
-SRR="SRRXXXXXX
-SRRXXXXXXX
-SRRXXXXXXX
-SRRXXXXXXX
-SRRXXXXXXX
-SRRXXXXXXX
+SRR="SRR7591064
+SRR7591067
+SRR7591065
+SRR7591066	
+SRR7591068
+SRR7591069
 "
 
-# For each SRR accession, download the data :
-for ... in ...
+for i in $SRR 
 do
+fastq-dump $i -X 4 
 
-fastq-dump -h
+# Ne pas oublier le $ pour pouvoir rentrer dans SRR (i dans boucle)
+# fastq-dump permet de récupérer les fichiers fastq depuis donnees de l'article 
+# -X 4: permet de selectionner 4 spots = coordonnées X,Y d'un read sur la plaque 
+#Illumina dans nos fichiers SRR
 
-# Rename sequence names, trinity need that their name ends with "/1".
 
-awk  '{ if (NR%2 == 1 ) {gsub("\\.","_");print $1"/1"}  else  { print $0}}' 
+
+awk  '{ if (NR%2 == 1 ) {gsub("\\.","_");print $1"/1"}  else  { print $0}}' $i".fastq" > $i"_2.fastq" 
+
+
+
+# awk permet de renommer noms de sequences en remplacant "." par "_" et ajouter /1 a la fin
+# nom de sequence doit finir par /1 car trinity peut fonctionner pour des sequencages en 
+#single read (/1) ou double read (deux sequences par read: /1 et /2)
+
+
+
+rm $i".fastq"
+mv $i"_2.fastq" $i."fastq"
+
+# remplace les fichiers .fastq de depart par les fichiers de sequence renommes ("_2.fastq"")
+
+
 
 done
+
 
