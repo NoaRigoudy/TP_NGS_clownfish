27cc2fc9b9ede1a686f851ee7febfd6178c67bbd
noatheclownfish <noa.rigoudy@ens-lyon.fr>
2019-11-06 14:53:43 +0000

    Fastqc: analyser qualite fichiers fastq

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-# Again go create / go in the working directory
+cd /home/rstudio/disk/data
+mkdir -p fastqc_data
+# Creation d'un dossier pour stocker nos donnees de fastqc
 
-# Create a folder for the fastqc
 
-# Pour chaque fastq 
-fastqc -h
+list_fqc=`ls sra_data/*.fastq`
+
+# Creation d'une liste qui contient tous les noms de fichiers .fastq 
+#que l'on veut analyser
+
+
+
+for i in $list_fqc
+do
+fastqc $i -o /home/rstudio/disk/data/fastqc_data
+done
+
+# Verification de qualite des fichiers fastq grace a fastqc
+#en utilisant une boucle for. On stocke dans un nouveau dossier fastqc_data
