a0a953f0ecc3fa1fbb5c1927fe7d920ee8369e3f
noatheclownfish <noa.rigoudy@ens-lyon.fr>
2019-11-06 14:54:27 +0000

    Multiqc: former un rapport combinant tous fastqc

--- src/multiqc.sh
+++ src/multiqc.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+cd /home/rstudio/disk/data
+# On se place dans le dossier data
 
-multiqc -h 
+multiqc fastqc_data -o /home/rstudio/disk/data/multiqc_data
 
+# On effectue un multiqc sur l'ensemble des fichiers contenus
+#dans le dossier fastqc_data et on les stocke dans un nouveau dossier multiqc_data
 
 
