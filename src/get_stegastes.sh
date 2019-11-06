337a75ea74a1c0cfb1db9432af52ee6e1e8bda31
noatheclownfish <noa.rigoudy@ens-lyon.fr>
2019-11-06 14:50:08 +0000

    Get stegastes: recuperer transcriptome de ref depuis Ensembl database

--- src/get_stegastes.sh
+++ src/get_stegastes.sh
@@ -1,8 +1,18 @@
-wget -O stegastes.fa 'http://ensembl.org/biomart/...
+cd /home/rstudio/disk/data/
+mkdir -p transdec_data
+cd transdec_data
+# Creation d'un dossier pour stocker donnees de Transdec
+
+wget -O stegastes.fa 'http://ensembl.org/biomart/martservice?query=
                         
-        <Dataset name = "..." interface = "default" >
-                <Attribute name = "..." />
-                <Attribute name = "..." />
-                <Attribute name = "..." />
-        </Dataset>
-</Query>'
+       <Query  virtualSchemaName = "default" formatter = "FASTA" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+				
+	<Dataset name = "spartitus_gene_ensembl" interface = "default" >
+		<Attribute name = "coding" />
+		<Attribute name = "external_gene_name" />
+		<Attribute name = "ensembl_gene_id" />
+	</Dataset>
+</Query> '
+
+# Telechargement de transcriptome de reference (Stegastes partitus) 
+#depuis database Ensembl
