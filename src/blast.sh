f044be13427db08648f01b675361361ff3f19e21
noatheclownfish <noa.rigoudy@ens-lyon.fr>
2020-01-06 12:01:02 +0000

    Checked and cleaned scripts Deseq_anal and blast

--- src/blast.sh
+++ src/blast.sh
@@ -8,17 +8,24 @@ cd $data/blast_data
 cd ..
 mv transdec_data/stegastes.fa blast_data
 #On bouge notre fichier .fa dans le dossier "blast_data"
+
 cd $data/blast_data
+
+
 
 PATH="$PATH:/softwares/ncbi-blast-2.9.0+/bin"
 #On ajoute "ncbi-blast-2.9.0+" dans les logiciels de la machine. Ne pas oublier /bin a la fin. 
 
+
 sed -re "s/\|//g" stegastes.fa > stegastes_clean.fa
 #On elimine les | des noms des sequences
 
-#run mart_to_blast.R pour retirer les duplicats du genome de reference
+#Run mart_to_blast.R pour retirer les duplicats du genome de reference
 
-# Build reference database
+
+
+# Build reference database for blastn
+
 
 makeblastdb -in /home/rstudio/disk/data/blast_data/stegastes_clean2.fa -parse_seqids -dbtype nucl -out stegastes
 
@@ -26,10 +33,16 @@ makeblastdb -in /home/rstudio/disk/data/blast_data/stegastes_clean2.fa -parse_se
 #-parse_seqids: permet de garder les identifiants de sequence du fichier .fa originel
 
 
+
 #Blast fasta against the ref db
+
 blastn -db stegastes -query /home/rstudio/disk/data/trinity_data/Trinity.fasta -evalue 1e-5 -outfmt 6 -out blast_stegastes
 
+#db: database=sequence reference contre laquelle on blast nos donnees
+#query: fichier contenant sequence a blaster
+#evalue: donne evalue minimum a considerer
+#outfmt: format du output
 
-Rscript --vanilla  annote_fasta_from_blast.R $out_blast 
+#Rscript --vanilla  annote_fasta_from_blast.R $out_blast 
 
 
--- src/deseq_anal.R
+++ src/deseq_anal.R
@@ -6,126 +6,117 @@ library("ggplot2")
 
 dir<-"/home/rstudio/disk/data/count_data"
 
-#Table de correspondance entre individu, SRR, type de tissu (orange,blanc)
+### Preparation pour Analyse DESeq
+
+
+## Table de correspondance entre individu, SRR, type de tissu (orange,blanc)
 
 samples <- read.table(file.path(dir,"samples.txt"), header=TRUE)
 
-#Creation d'une table de correspondance gene transcrit
+## Creation d'une table de correspondance gene transcrit
 
 tx2gene=read.table(file="/home/rstudio/disk/data/trinity_data/Trinity.fasta.gene_trans_map")
-#on utilise la table du output de Trinity gene_trans_map
+#On utilise la table du output de Trinity gene_trans_map
 
 
 tx2gene=data.frame(tx2gene$V2, tx2gene$V1)
 names(tx2gene)=c("Transcrit","Gene")
-#on echange les colonnes car tximport necessite d'avoir les transcrits en premiere colonne
+#On echange les colonnes dans notre tableau car tximport necessite d'avoir les transcrits 
+#en premiere colonne
 
 files <- file.path(dir,paste(samples$SRR,".fastq", sep=""),"quant.sf")
 names(files) <- samples$SRR
-# creation d'une variable condition qui contient:
+#Creation d'une variable condition qui contient:
 #dir: le chemin de travail
 #samples$SRR (il faut rajouter .fastq et enlever les separateurs car les noms de dossiers 
 #du count_data contiennent .fastq)
 #quant.sf: le fichier output de salmon
 
+
+## Import des donnees de quantification dans R
+
 txi <- tximport(files, type="salmon", tx2gene=tx2gene)
-#importe les donnees de quantification dans R
+
+
+
+## Creation d'une table des comptes pour l'analyse DESeq (DESeqDataSet)
+#a partir des donnees de quantification.
 
 library("DESeq2")
 ddsTxi <- DESeqDataSetFromTximport(txi,
                                    colData = samples,
                                    design = ~ Indiv + Tissue)
 
-# Creation d'une table des comptes pour l'analyse DESeq (DESeqDataSet)
-#a partir des donnees de quantification.
-# On utilise un design Indiv + Tissue pour prendre en compte dans notre analyse
-#l'effet tissue en sachant l'individu.
+#On utilise un design Indiv + Tissue pour prendre en compte dans notre analyse
+#de l'effet tissu en sachant l'individu.
 
-# Pre-filtering des donnees 
+
+
+## Pre-filtering des donnees 
 
 keep <- rowSums(counts(ddsTxi)) >= 10
 dds <- ddsTxi[keep,]
-# Permet d'eliminer les genes qui n'ont pas assez de reads, leur analyse utiliserait
-#de la memoire pour rien car leur analyse ne serait pas possible
+#Permet d'eliminer les genes qui n'ont pas assez de reads, leur analyse utiliserait
+#de la memoire pour rien car elle ne serait pas possible au vu de leur nombre faible
 
-# Assigner des niveaux de facteurs
+
+
+## Assigner des niveaux de facteurs
 
 dds$Tissue <- factor(dds$Tissue, levels = c("O","W"))
-#on precise que orange (O) est la valeur "temoin"
+#On precise que orange (O) est la valeur "temoin"
 
-#DESeq Analysis
+
+
+
+### Analyse DESeq
+
+##Analysis 
 
 dds <- DESeq(dds)
 res <- results(dds)
 summary(res)
+#Summary des resultats de l'analyse DESeq
 
 res2<- results(dds, alpha=0.05)
 summary(res2)
+#Summary des resultats de l'analyse DESeq seulement pour les points avec des p-adj<0.05
+
 head(res2)
-# summary des resultats de l'analyse DESeq seulement pour les points avec des p-adj<0.05 
-# Analyse DESeq de notre table des comptes: 
 #baseMean: valeurs d'expression normalisees
 #pvalue et padj: significativite de la difference d'expression de chaque gene entre les 2 tissus
 
-#Log fold change shrinkage
+
+## Log fold change shrinkage
+
 resultsNames(dds)
 resLFC <- lfcShrink(dds, coef="Tissue_W_vs_O", type="apeglm")
 summary(resLFC)
+#Summary sur les donnees shrinkees ce qui permet de reduire le bruit genere par 
+#par les genes a faible copies
 
 sum(resLFC$padj < 0.05, na.rm=TRUE)
-#somme des genes qui ont une padj<0.05, on trouve 501
+#Somme des genes qui ont une padj<0.05, on en trouve 501
 
-geneID_Trinity = read.table("/home/rstudio/disk/data/count_data/Aoce_transdecoder.stegastes.tsv", h=T)
-geneID_Trinity$id=gsub("_i.*","",geneID_Trinity$Aoce)
 
-resLFC$id=rownames(resLFC)
-table_resLFC=data.frame(resLFC)
+## PC Analysis
 
-Goku = merge(Table_resLFC, geneID_Trinity,by.x="id",by.y="id" )
-head(Goku)
-
-#Search for Saiyan
-
-Goku[Goku$Steg_ref=="ENSSPAG00000013419|si:ch211-256m1.8", ]
-
-#Count data for Saiyan
-
-txi$counts["TRINITY_DN20767_c0_g1",]
-
-#plot les counts
-saiyan_plot <- plotCounts(dds, gene="TRINITY_DN20767_c0_g1", intgroup="Tissue", 
-                          returnData=TRUE)
-library("ggplot2")
-ggplot(saiyan_plot, aes(x=Tissue, y=count)) + 
-  geom_point(position=position_jitter(w=0.1,h=0)) + 
-  scale_y_log10(breaks=c(25,100,400))
-
-#Count data for TOP 10 genes compared to paper
-#we get the gene names on ensembl and thanks to annote_fasta_from_blast.R script
-
-#gene fhl2a
-blast_stegastes<-read.table("/home/rstudio/disk/data/blast_data/blast_stegastes", h=F) 
-blast_stegastes[blast_stegastes$V2 %in% "ENSSPAG00000006760.1fhl2b" ,]
-#on trouve l'id Ensembl en cherchant les orthologues du gene du zebrafish chez stegastes 
-#fhl2a dans le blast: l'id Trinity est 
-
-# PC Analysis
-
-#Transformation des donnees de compte pour la PCA
+# Transformation des donnees de comptes pour la PCA
 
 rld <- rlog(dds, blind=FALSE)
 #ici on utilise rlog qui permet de prendre considere des differences a priori
 #dans nos comptes selon le tissu
 head(assay(rld), 3)
 
-#PCA data and plot
+# PCA data and plot
 
 PCA_data1<-plotPCA(rld, intgroup=c("Tissue", "Indiv"), returnData=T)
 PCA_data2<-plotPCA(rld, intgroup=c("Tissue"))
 PCA_data3<-plotPCA(rld, intgroup=c("Indiv"))
+#Permet de visualiser les principaux parametres expliquant la variance observee
 
 percentVar <- round(100 * attr(PCA_data1, "percentVar"))
-#recupere les pourcentages de variation expliques par PC1 et PC2
+#Recupere les pourcentages de variation expliques par PC1 et PC2
 
 ggplot(PCA_data1, aes(PC1, PC2, color=Tissue, shape=as.character(Indiv))) +
   geom_point(size=3) +
@@ -137,11 +128,71 @@ ggplot(PCA_data1, aes(PC1, PC2, color=Tissue, shape=as.character(Indiv))) +
 #Attention, Indiv a transformer par as.character car ils sont numerotes et reconnus 
 #comme variable continue
 
-#MA Plot
+
+## MA Plot
 
 plotMA(res, ylim=c(-5,5))
 plotMA(resLFC, ylim=c(-5,5))
 #Distribution des variances 
+
+
+
+
+### Search for Saiyan
+
+
+
+geneID_Trinity = read.table("/home/rstudio/disk/data/count_data/Aoce_transdecoder.stegastes.tsv", h=T)
+geneID_Trinity$id=gsub("_i.*","",geneID_Trinity$Aoce)
+#Recuperation des correspondances entre identite de transcrit et read dans le fichier Trinity
+
+
+resLFC$id=rownames(resLFC)
+table_resLFC=data.frame(resLFC)
+#Creation d'une table contenant les resultats de l'analyse differentielle DeSeq
+
+
+Goku = merge(Table_resLFC, geneID_Trinity,by.x="id",by.y="id" )
+head(Goku)
+#Creation d'une table conmbinant les correspondances entre gene blaste, transcrit et nos  
+#valeurs pour l'analyse d'expression differentielle pour avoir les niveaux d'expression 
+#pour chaque transcrit identifie.
+
+
+
+Goku[Goku$Steg_ref=="ENSSPAG00000013419|si:ch211-256m1.8", ]
+#On cherche les niveaux d'expression de notre gene d'interet Saiyan grace a son identifiant Ensembl 
+#dans notre table Goku
+
+
+
+## Count data for Saiyan
+
+txi$counts["TRINITY_DN20767_c0_g1",]
+#Cherche les comptes pour le gene Saiyan en particulier
+
+#Plot des comptes
+
+saiyan_plot <- plotCounts(dds, gene="TRINITY_DN20767_c0_g1", intgroup="Tissue", 
+                          returnData=TRUE)
+library("ggplot2")
+ggplot(saiyan_plot, aes(x=Tissue, y=count)) + 
+  geom_point(position=position_jitter(w=0.1,h=0)) + 
+  scale_y_log10(breaks=c(25,100,400))
+
+
+#Comparaison des donnees de comptes pour notre TOP 10 de genes differentiellement exprimes, 
+# a comparer avec le papier
+
+
+#On recupere les noms des genes sur Ensembl et grace au script annote_fasta_from_blast.R 
+
+#gene fhl2a
+blast_stegastes<-read.table("/home/rstudio/disk/data/blast_data/blast_stegastes", h=F) 
+blast_stegastes[blast_stegastes$V2 %in% "ENSSPAG00000006760.1fhl2b" ,]
+#on trouve l'id Ensembl en cherchant les orthologues du gene du zebrafish chez stegastes 
+#fhl2a dans le blast: l'id Trinity est 
+
 
 
 
